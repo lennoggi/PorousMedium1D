e84eafc3afd89880ec6de3fa3188731da4272159
Lorenzo Ennoggi <lorenzo.ennoggi@gmail.com>
2026-04-02 11:58:33 +0200

    Initial_data.cc : add 0 after decimal point everywhere

--- Initial_data.cc
+++ Initial_data.cc
@@ -6,8 +6,8 @@ using namespace std;
 
 // Gaussian
 double gaussian(const double &x) {
-    constexpr double norm   = 1./(SIGMA*sqrt(2.*M_PI));
-    constexpr double expfac = 1./(2*SIGMA*SIGMA);
+    constexpr double norm   = 1.0/(SIGMA*sqrt(2.0*M_PI));
+    constexpr double expfac = 1.0/(2.0*SIGMA*SIGMA);
     const     double xs     = x - X0;
     return norm*exp(-expfac*xs*xs);
 }
